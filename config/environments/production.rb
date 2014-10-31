8432acd075e44579bdfb42d2868ca81ea77386c6
Chris Lerum <chris.lerum@gmail.com>
2014-10-30 23:46:50 -0400

    x

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.action_mailer.default_url_options = { host: 'pigglecraft.com' }
   # Generate digests for assets URLs.
   config.assets.digest = true
 
