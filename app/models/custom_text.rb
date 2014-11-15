6439d157024578cdf06c653e7ce9b70647222856
Chris Lerum <chris.lerum@gmail.com>
2014-11-15 12:00:28 -0500

    x

--- app/models/custom_text.rb
+++ app/models/custom_text.rb
@@ -1,3 +1,14 @@
 class CustomText < ActiveRecord::Base
   validates :name, :the_text, presence: true
+
+  rails_admin do
+
+    object_label_method do
+      :custom_label_method
+    end
+  end
+
+  def custom_label_method
+    self.name
+  end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,22 @@
 class Report < ActiveRecord::Base
   mount_uploader :screenshot, ScreenshotUploader
   validates :player, :description, presence: true
+
+  rails_admin do
+
+    object_label_method do
+      :custom_label_method
+    end
+
+    list do
+      field :player
+      field :description
+      field :screenshot
+    end
+  end
+
+  def custom_label_method
+    self.player
+  end
+
 end
